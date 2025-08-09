77cec198224c8ad323b17fdbaf2a726c6b47dc1a
rjldg <rjlimdeguzman@gmail.com>
2025-08-09 12:13:10 +0800

    added libs and env files

---
+++ fp.py
@@ -0,0 +1,19 @@
+Aluminium foil
+Bottle
+Bottle cap
+Broken glass
+Can
+Carton
+Cigarette
+Cup
+Lid
+Other litter
+Other plastic
+Paper
+Plastic bag & wrapper
+Plastic bag - wrapper
+Plastic container
+Pop tab
+Straw
+Styrofoam piece
+Unlabeled litter
